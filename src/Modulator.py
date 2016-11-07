bd93e600e4a2fa401ebf8039af80f697b15d7174
lvanderlyn <lindsey.vanderlyn@students.olin.edu>
2016-11-06 23:52:02 -0500

    added code to get the spectral window by finding peaks and interpolating from them with a moving average

--- src/Modulator.py
+++ src/Modulator.py
@@ -1,10 +1,91 @@
+import sys
+sys.path.insert(0, '../lib')
 import wave
+import thinkdsp as td
+import thinkplot as tp
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import hilbert, chirp
+import scipy
+import peakutils
+from scipy.interpolate import interp1d
+import math
 
 class Modulator(object):
     """docstring for Modulator"""
     def __init__(self, file_name):
         self.text_grid = TextGrid()
-        self.text_grid.parseFile(file_name)
+        self.text_grid.parse_file(file_name)
+
+
+    def get_envelope(self):
+        wave = td.read_wave('outTest.wav')
+        # fig1 = plt.figure(1)
+        # plt.subplot(211)
+        # plt.plot(wave.ts, wave.ys)
+        # plt.title('Original Wave Clip')
+
+        hanning = np.hanning(len(wave))
+
+        hanwave = wave
+        hanwave.window(hanning)
+
+        ##frequency domain plotting stuff
+        spec = wave.make_spectrum()
+        hanspec = hanwave.make_spectrum()
+
+        ys = hanspec.amps
+        xs = hanspec.fs
+        indexes = peakutils.indexes(ys, thres=0.02/max(ys), min_dist=15)
+
+        env_xs, env_ys = interpolate(indexes, xs, ys)
+
+        fig4 = plt.figure()
+        plt.plot(spec.fs, spec.amps, label="signal")
+        plt.plot(env_xs, env_ys, label='window')
+        plt.plot([spec.fs[i] for i in indexes], [spec.amps[i] for i in indexes], label='peaks')
+
+        plt.legend()
+        plt.xlim([0,8000])
+
+        ##Low pass filter the analytic signal
+        N=10
+        Fc=40
+        Fs=1600
+        # provide them to firwin
+        h=scipy.signal.firwin( numtaps=N, cutoff=2, nyq = 800)
+        # filtEnve=scipy.signal.lfilter( h, 1.0, hanspec.amps) # 'x' is the time-series data you are filtering
+
+        # plt.plot(spec.fs, filtEnve, label='envelope')
+        # plt.plot(spec.fs, spec.amps/filtEnve, label='envelope')
+
+
+
+        #Trying to use peaks to generate envelope
+        peaks = scipy.signal.find_peaks_cwt(hanspec.amps, np.arange(1,100))
+        # plt.plot(peaks, hanspec.amps[peaks])
+        plt.yscale('log')
+        plt.show()
+
+def interpolate(indexes, fs, amps):
+    indexes = [0] + [i for i in indexes]
+    ret_val = np.ones(len(fs))
+    ret_val = [n for n in ret_val]
+
+    for i in range(len(indexes)-1):
+        index1 = indexes[i]
+        index2 = indexes[i+1]
+
+        rise = int(amps[index2] - amps[index1])
+        run = int(fs[index2] - fs[index1])
+        k = float(rise/float(run))
+        c = amps[index1]
+
+        ret_val[index1:index2] = [(k*x) + c for x in range(0, index2-index1)]
+    N = 30
+    ys = np.convolve(ret_val, np.ones((N))/N, mode='valid')
+
+    return fs[:len(ys)], ys
 
 
 
@@ -21,7 +102,7 @@ class TextGrid(object):
         self.end_time = x_max
         self.tiers = tiers
 
-    def parseFile(self, file_name):
+    def parse_file(self, file_name):
         with open(file_name) as f:
             content = f.readlines()
         self.start_time = content[3]
@@ -37,23 +118,23 @@ class TextGrid(object):
                 tier_1.append(line)
             else:
                 tier_2.append(line)
-        inf1 = self.parseTier(tier_1)
-        inf2 = self.parseTier(tier_2)
+        inf1 = self.parse_tier(tier_1)
+        inf2 = self.parse_tier(tier_2)
         self.tiers = dict([(inf1[0], inf1[1]), (inf2[0], inf2[1])])
 
-    def parseTier(self, lines):
+    def parse_tier(self, lines):
         name = lines[1][10:-2] 
         intervals = []
         interval = []
         for line in lines[6:]:
             if "interval" in line:
-                intervals.append(self.parseInterval(interval))
+                intervals.append(self.parse_interval(interval))
                 interval = []
             else:
                 interval.append(line)
         return name, intervals
     
-    def parseInterval(self, interval):
+    def parse_interval(self, interval):
         return dict([("start", float(stripWhite(interval[0]))), ("end", float(stripWhite(interval[1]))), ("name", stripWhite(interval[2]))])
 
 """
@@ -84,10 +165,12 @@ def stripWhite(string):
     
 
 if __name__ == '__main__':
-    # n = Modulator("../test/english44clipped.TextGrid")
-    # words = n.text_grid.tiers["words"]
-    # start = words[1]["start"]
-    # end = words[1]["end"]
+    n = Modulator("../test/english44clipped.TextGrid")
+    n.get_envelope()
+    # n.cepstral_env_estimate()
+    # words = n.text_grid.tiers["phones"]
+    # start = words[2]["start"]
+    # end = words[2]["end"]
     # print(start, end)
-    slice_wav("../test/english44clipped.wav", "outTest.wav", int(2*1000), int(3*1000))
+    # slice_wav("../test/english44clipped.wav", "outTest.wav", int(start*1000), int(end*1000))
 
