bf61a15ad51e6c9fea8f14a735137b232a74de3b
Jiaying Wei <jywei02@gmail.com>
2016-12-01 20:13:56 -0500

    uploading audio and UI code

--- src/handle_keyboard_input.py
+++ src/handle_keyboard_input.py
@@ -1,116 +1,64 @@
+import pyaudio  
+import wave  
+import pygame
+import time
+
 class Controller:
     '''controls the jumpman's motion'''
-    def __init__(self,model):
-        '''initializes the model'''
-        self.model = model
+    # def __init__(self,model):
+    #     '''initializes the model'''
+    #     self.model = model
     
     def handleEvent(self, event):
-        '''Defines all scenarios that can happen to jumpman and movements associated with said states
-            Takes in arrow keys and WASD input, and spacebar to jump'''
-        if event.type == QUIT:
-            pygame.quit()
+        '''Defines all scenarios that can happen to the Transpose controller'''
         pressed = pygame.key.get_pressed() #finds keys pressed so multiple input can be taken
-        if self.model.mode == MODE_UNDERLADDER: 
-            #should be able to move up, left, or right, jump
-            self.model.jumpman.vx = 0
-            self.model.jumpman.vy = 0
-            if pressed[K_LEFT] or pressed[ord('a')]:
-                self.model.jumpman.vx = -1.0*MOVESPEED
-                walk.play()
-            if pressed[K_RIGHT] or pressed[ord('d')]:
-                self.model.jumpman.vx = 1.0*MOVESPEED
-                walk.play()
-            if pressed[K_UP] or pressed[ord('w')]:
-                self.model.jumpman.vy = -1.0*MOVESPEED
-                walk.play()
-            if pressed[K_SPACE]:
-                jumpNoise.play()
-                self.model.jumpman.jump()
-        elif self.model.mode == MODE_ABOVELADDER:
-            #should be able to move, down, left, right, jump
-            self.model.jumpman.vx = 0
-            self.model.jumpman.vy = 0
-            if pressed[K_LEFT] or pressed[ord('a')]:
-                self.model.jumpman.vx = -1.0*MOVESPEED
-                walk.play()
-            if pressed[K_RIGHT] or pressed[ord('d')]:
-                self.model.jumpman.vx = 1.0*MOVESPEED
-                walk.play()
-            if pressed[K_DOWN] or pressed[ord('s')]:
-                self.model.jumpman.vy = 1.0*MOVESPEED
-                walk.play()
-            if pressed[K_SPACE]:
-                jumpNoise.play()
-                self.model.jumpman.jump()
-        elif self.model.mode == MODE_ONLADDER:        
-            #should be able to move up, down
-            self.model.jumpman.vx = 0
-            self.model.jumpman.vy = 0
-            if pressed[K_LEFT] or pressed[ord('a')]:
-                self.model.jumpman.vx = -1.0*MOVESPEED
-                walk.play()
-            if pressed[K_RIGHT] or pressed[ord('d')]:
-                self.model.jumpman.vx = 1.0*MOVESPEED
-                walk.play()
-            if pressed[K_DOWN] or pressed[ord('s')]:
-                self.model.jumpman.vy = 1.0*MOVESPEED
-                walk.play()
-            if pressed[K_UP] or pressed[ord('w')]:
-                self.model.jumpman.vy = -1.0*MOVESPEED
-                walk.play()
-            if pressed[K_SPACE]:
-                jumpNoise.play()
-                self.model.jumpman.jump()
-        elif self.model.mode == MODE_ONPLATFORM:
-            #should be able to move left, right, jump
-            self.model.jumpman.vx = 0
-            self.model.jumpman.vy = 0
-            if pressed[K_LEFT] or pressed[ord('a')]:
-                self.model.jumpman.vx = -1.0*MOVESPEED
-                walk.play()
-            if pressed[K_RIGHT] or pressed[ord('d')]:
-                self.model.jumpman.vx = 1.0*MOVESPEED
-                walk.play()
-            if pressed[K_SPACE]:
-                jumpNoise.play()
-                self.model.jumpman.jump()
-        elif self.model.mode == MODE_FALLING:
-            #should be able to move left, right
-            self.model.jumpman.vx = 0
-            if pressed[K_LEFT] or pressed[ord('a')]:
-                self.model.jumpman.vx = -1.0*MOVESPEED
-                walk.play()
-            if pressed[K_RIGHT] or pressed[ord('d')]:
-                self.model.jumpman.vx = 1.0*MOVESPEED
-                walk.play()
-        elif self.model.mode == MODE_UPDOWNLADDER:
-            #should be able to move up down left right jump
-            self.model.jumpman.vx = 0
-            self.model.jumpman.vy = 0
-            if event.type != KEYDOWN:
-                return
-            if pressed[K_LEFT] or pressed[ord('a')]:
-                self.model.jumpman.vx = -1.0*MOVESPEED
-                walk.play()
-            if pressed[K_RIGHT] or pressed[ord('d')]:
-                self.model.jumpman.vx = 1.0*MOVESPEED
-                walk.play()
-            if pressed[K_UP] or pressed[ord('w')]:
-                self.model.jumpman.vy = -1.0*MOVESPEED
-                walk.play()
-            if pressed[K_DOWN] or pressed[ord('s')]:
-                self.model.jumpman.vy = 1.0*MOVESPEED
-                walk.play()
-            if pressed[K_SPACE]:
-                self.model.jumpman.jump()
-                jumpNoise.play()
+        # if self.model.mode == MODE_INSTRUCT: 
+            # if pressed[K_s]:
+            #     #play instructions recording
+            #     #set voice = nothing
+        print(pressed)
+        if pressed[K_q]:
+            pygame.quit()
+
+        if pressed[K_1]:
+            #PLAY PERSON1 INTRO
+            #set voice = to PERSON1
+            Jee_Intro= pygame.mixer.Sound("../intros/Jee_Intro.wav")
+            Jee_Intro.play()
+
+        if pressed[K_2]:
+            Pinar_Intro= pygame.mixer.Sound("../intros/Pinar_Intro.wav")
+            Pinar_Intro.play()  
+            # if pressed[K_3]:
+
+            # if pressed[K_4]:
+            # if pressed[K_5]:
+            # if pressed[K_6]:
+
+            # if pressed[K_r]:
+            #     #start recording 
+            # if pressed[K_t]:
+            #     #stop recording
+            #     #save recording and send it through modulator
+            #     #save the modulator sound as a .wav
+            #     #play the recording
+            #     #play the outro
+        # elif self.model.mode == MODE_PLAYBACK:
+        #     if pressed[K_f]:
+        #         #replay the stored .wavfile
+        #     if pressed[K_s]:
+        #         #play instructions recording
+        #         #set voice = nothing
+        #         self.model.mode == MODE_INSTRUCT
+
             
 if __name__ == '__main__':
-    '''THE MAIN GAME STUFF!'''
+    '''THE MAIN STUFF!'''
     controller = Controller()
+    pygame.init()
+    pygame.mixer.init()
     pygame.time.wait(2500)
     running = True
-    #this loop is the running loop within a single game level
     while running:
         for event in pygame.event.get():
             if event.type == QUIT:
