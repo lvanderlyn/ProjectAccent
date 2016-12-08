ef79d4852707a8df80dcab774d538420a8d9c82d
lvanderlyn <lindsey.vanderlyn@students.olin.edu>
2016-12-08 01:38:10 -0500

    now with correct looping and better keymapping - still need to add Daniela somewhere

--- src/brick_breaker_section_1.py
+++ src/brick_breaker_section_1.py
@@ -12,6 +12,7 @@ import math
 import time
 import wave
 import pyaudio
+import time
 sys.path.insert(0, '../lib')
 # import thinkdsp as td
 
@@ -35,112 +36,154 @@ class PyGameKeyboardController:
         selection: picking a voice
         playback: allows playback after transformations
         '''
-        self.state = 'selection'
-        self.voice = 'none'
+        self.mode = "intro"
+        self.button = 'none'
+        self.user_num = 0
+        self.start_time = time.time()
 
     def handle_pygame_event(self, event):
-        if event.type != KEYDOWN:
-                return
 
-        if self.state == 'selection':
-            if event.key == pygame.K_w:
-                self.voice = 'none'
-                print('Someone is saying instructions right now')
-                # Instructions = pygame.mixer.Sound()
+        if self.mode == "intro":
+            if event.type == KEYDOWN:
+                if event.key == pygame.K_d:
+                    self.button = 'Start'
+                    print('Intro')
+                    sound = pygame.mixer.Sound("../poem/Intro.wav")
+                    sound.play()
+                    self.mode = "record"
+                    self.start_time = time.time()
 
-            if event.key == pygame.K_a:
-                poem = pygame.mixer.Sound("../poem/Bruce_Poem.wav")
-                poem.play()
-                self.voice = 'Bruce'
-                print(self.voice)
-     
-            if event.key == pygame.K_s:
-                poem = pygame.mixer.Sound("../poem/James_Poem.wav")
-                poem.play()
-                self.voice = 'James'
-                print(self.voice)
+        if self.mode == "record":
+            if event.type == KEYDOWN:
+                if event.key == pygame.K_f:
+                    if self.button == 'none':
+                        pass
+                    else:
+                        print("start recording - press stop to terminate recording")
+                        CHUNK = 1024 
+                        FORMAT = pyaudio.paInt16 #paInt8
+                        CHANNELS = 2 
+                        RATE = 44100 #sample rate
+                        RECORD_SECONDS = 20
+                        WAVE_OUTPUT_FILENAME = "voice{0}.wav".format(self.user_num)
+                        self.user_num = (self.user_num + 1) % 3
 
-            if event.key == pygame.K_d:
-                poem = pygame.mixer.Sound("../poem/Jee_Poem.wav")
-                poem.play()
-                self.voice = 'Jee'
-                print(self.voice)
+                        p = pyaudio.PyAudio()
 
-            if event.key == pygame.K_f:
-                poem = pygame.mixer.Sound("../poem/Jiaying_Poem.wav")
-                poem.play()
-                self.voice = 'Jiaying'
-                print(self.voice)
+                        stream = p.open(format=FORMAT,
+                                        channels=CHANNELS,
+                                        rate=RATE,
+                                        input=True,
+                                        frames_per_buffer=CHUNK) #buffer
 
-            if event.key == pygame.K_g:
-                poem = pygame.mixer.Sound("../poem/Pinar_Poem.wav")
-                poem.play()
-                self.voice = 'Pinar'
-                print(self.voice)
+                        print("* recording")
 
-            if event.key == pygame.K_6:
-                poem = pygame.mixer.Sound("../poem/Dimitar_Poem.wav")
-                poem.play()
-                self.voice = 'Dimitar'
-                print(self.voice)
+                        frames = []
 
-            if event.key == pygame.K_r:
-                if self.voice == 'none':
-                    pass
-                else:
-                    print("start recording - press T to terminate recording")
-                    CHUNK = 1024 
-                    FORMAT = pyaudio.paInt16 #paInt8
-                    CHANNELS = 2 
-                    RATE = 44100 #sample rate
-                    RECORD_SECONDS = 20
-                    WAVE_OUTPUT_FILENAME = "learner.wav"
+                        done = False
+                        while(done == False):
+                            data = stream.read(CHUNK)
+                            frames.append(data)
+                            for newevent in pygame.event.get():
+                                if newevent.type == KEYDOWN and newevent.key == pygame.K_g:
+                                    done = True
 
-                    p = pyaudio.PyAudio()
+                        print ("finished recording")
+                        stream.stop_stream()
+                        stream.close()
+                        p.terminate()
 
-                    stream = p.open(format=FORMAT,
-                                    channels=CHANNELS,
-                                    rate=RATE,
-                                    input=True,
-                                    frames_per_buffer=CHUNK) #buffer
+                        wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+                        wf.setnchannels(CHANNELS)
+                        wf.setsampwidth(p.get_sample_size(FORMAT))
+                        wf.setframerate(RATE)
+                        wf.writeframes(b''.join(frames))
+                        wf.close()  
 
-                    print("* recording")
+                        self.mode = "play"              
+                        self.start_time = time.time()
 
-                    frames = []
 
-                    done = False
-                    while(done == False):
-                        data = stream.read(CHUNK)
-                        frames.append(data)
-                        for newevent in pygame.event.get():
-                            if newevent.type == KEYDOWN and newevent.key == pygame.K_t:
-                                done = True
+        if self.mode == "play":
+            if  event.type == pygame.MOUSEBUTTONDOWN:
 
-                    print ("finished recording")
-                    stream.stop_stream()
-                    stream.close()
-                    p.terminate()
+                if event.button == 1: #left click
+                    poem = pygame.mixer.Sound("../poem/Pinar_Poem.wav")
+                    poem.play()
+                    self.button = 'Pinar'
+                    print(self.button)
+                    self.start_time = time.time()                    
 
-                    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-                    wf.setnchannels(CHANNELS)
-                    wf.setsampwidth(p.get_sample_size(FORMAT))
-                    wf.setframerate(RATE)
-                    wf.writeframes(b''.join(frames))
-                    wf.close()
-                #send the following to modulator teacher = self.voice's mp3, learner = recorded audio
-                #play the modulated voice
-                self.state = 'playback'
-        if self.state == 'playback':
-            if event.key == pygame.K_f:
-                Replay = pygame.mixer.Sound("learner.wav")
-                Replay.play()
-                print('Hearing your modified voice')
-            if event.key == pygame.K_s:
-                self.voice = 'none'
-                print('Someone is saying instructions right now')
-                # Instructions = pygame.mixer.Sound()
-                self.state = 'selection'
-                self.voice = 'none'
+                if event.button == 3: #right click
+                    poem = pygame.mixer.Sound("../poem/Dimitar_Poem.wav")
+                    poem.play()
+                    self.button = 'Dimitar'
+                    print(self.button)
+                    self.start_time = time.time()
+
+
+            if event.type == KEYDOWN:
+
+                if event.key == pygame.K_d:
+                    self.button = 'Start'
+                    print('Intro')
+                    sound = pygame.mixer.Sound("../poem/Intro.wav")
+                    sound.play()
+                    self.mode = "record"
+                    self.start_time = time.time()
+
+                if event.key == pygame.K_w:
+                    poem = pygame.mixer.Sound("../poem/voice0.wav")
+                    poem.play()
+                    self.button = 'V1'
+                    print(self.button)
+                    self.start_time = time.time()                    
+
+                if event.key == pygame.K_a:
+                    poem = pygame.mixer.Sound("../poem/voice1.wav")
+                    poem.play()
+                    self.button = 'V2'
+                    print(self.button)
+                    self.start_time = time.time()                    
+
+                if event.key == pygame.K_s:
+                    poem = pygame.mixer.Sound("../poem/voice2.wav")
+                    poem.play()
+                    self.button = 'V3'
+                    print(self.button)
+                    self.start_time = time.time()                    
+
+                if event.key == pygame.K_DOWN:
+                    poem = pygame.mixer.Sound("../poem/Bruce_Poem.wav")
+                    poem.play()
+                    self.button = 'Bruce'
+                    print(self.button)
+                    self.start_time = time.time()         
+
+                if event.key == pygame.K_UP:
+                    poem = pygame.mixer.Sound("../poem/James_Poem.wav")
+                    poem.play()
+                    self.button = 'James'
+                    print(self.button)
+                    self.start_time = time.time()
+
+                if event.key == pygame.K_RIGHT:
+                    poem = pygame.mixer.Sound("../poem/Jee_Poem.wav")
+                    poem.play()
+                    self.button = 'Jee'
+                    print(self.button)
+                    self.start_time = time.time()                    
+
+                if event.key == pygame.K_LEFT:
+                    poem = pygame.mixer.Sound("../poem/Jiaying_Poem.wav")
+                    poem.play()
+                    self.button = 'Jiaying'
+                    print(self.button)
+                    self.start_time = time.time()                    
+
+
+                
+
 
 
 
@@ -155,11 +198,18 @@ if __name__ == '__main__':
     view = PyGameBrickBreakerView(None,screen)
     controller = PyGameKeyboardController(None)
     running = True
+    start_time = time.time()
     while running:
+        d_time = time.time() - controller.start_time
+
+        if d_time > 45:
+            controller.mode = "intro"
+
         for event in pygame.event.get():
             if event.type == QUIT:
                 running = False
             controller.handle_pygame_event(event)
+            start_time = time.time()
         time.sleep(.001)
 
     pygame.quit()
