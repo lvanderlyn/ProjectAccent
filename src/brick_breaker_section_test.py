efcead8627b6b46a88536273d30a86c844552a1f
lvanderlyn <lindsey.vanderlyn@students.olin.edu>
2016-12-12 20:34:31 -0500

    fixed so multiple button press is no longer possible

--- src/brick_breaker_section_test.py
+++ src/brick_breaker_section_test.py
@@ -42,159 +42,150 @@ class PyGameKeyboardController:
         self.start_time = time.time()
 
     def handle_pygame_event(self, event):
-        print(self.mode)
-        if self.mode == "busy":
-            return
-        else:
-            if self.mode == "intro":
-                if event.type == KEYDOWN:
-                    if event.key == pygame.K_d:
-                        self.button = 'Start'
-                        print('Intro')
-                        self.mode = "busy"
-                        sound = pygame.mixer.Sound("../poem/Intro.wav")
-                        sound.play()
-                        self.mode = "record"
-                        self.start_time = time.time()
 
-            if self.mode == "record":
-                if event.type == KEYDOWN:
-                    if event.key == pygame.K_f:
-                        if self.button == 'none':
-                            pass
-                        else:
-                            print("start recording - press stop to terminate recording")
-                            self.mode == "busy"
-                            CHUNK = 1024 
-                            FORMAT = pyaudio.paInt16 #paInt8
-                            CHANNELS = 2 
-                            RATE = 48000 #sample rate
-                            RECORD_SECONDS = 20
-                            WAVE_OUTPUT_FILENAME = "../poem/voice{0}.wav".format(self.user_num)
-                            self.user_num = (self.user_num + 1) % 3
+        print("MODE", self.mode)
+        if self.mode == "intro":
+            if event.type == KEYDOWN:
+                if event.key == pygame.K_d:
+                    self.button = 'Start'
+                    print('Intro')
+                    self.mode = "busy"
+                    sound = pygame.mixer.Sound("../poem/Intro.wav")
+                    sound.play()
+                    time.sleep(sound.get_length())
+                    self.mode = "record"
+                    self.start_time = time.time()
 
-                            p = pyaudio.PyAudio()
-
-                            stream = p.open(format=FORMAT,
-                                            channels=CHANNELS,
-                                            rate=RATE,
-                                            input=True,
-                                            frames_per_buffer=CHUNK) #buffer
-
-                            print("* recording")
-
-                            frames = []
-
-                            done = False
-                            while(done == False):
-                                data = stream.read(CHUNK)
-                                frames.append(data)
-                                for newevent in pygame.event.get():
-                                    if newevent.type == KEYDOWN and newevent.key == pygame.K_g:
-                                        done = True
-
-                            print ("finished recording")
-                            stream.stop_stream()
-                            stream.close()
-                            p.terminate()
-
-                            wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-                            wf.setnchannels(CHANNELS)
-                            wf.setsampwidth(p.get_sample_size(FORMAT))
-                            wf.setframerate(RATE)
-                            wf.writeframes(b''.join(frames))
-                            wf.close()  
-
-                            self.mode = "play"              
-                            self.start_time = time.time()
-
-
-            if self.mode == "play":
-                if  event.type == pygame.MOUSEBUTTONDOWN:
-
-                    if event.button == 1: #left click
+        if self.mode == "record":
+            if event.type == KEYDOWN:
+                if event.key == pygame.K_f:
+                    if self.button == 'none':
+                        pass
+                    else:
+                        print("start recording - press stop to terminate recording")
                         self.mode == "busy"
-                        poem = pygame.mixer.Sound("../poem/Pinar_Poem.wav")
-                        poem.play()
-                        self.button = 'Pinar'
-                        print(self.button)
-                        self.mode = "play"
-                        self.start_time = time.time()                    
+                        CHUNK = 1024 
+                        FORMAT = pyaudio.paInt16 #paInt8
+                        CHANNELS = 2 
+                        RATE = 48000 #sample rate
+                        RECORD_SECONDS = 20
+                        WAVE_OUTPUT_FILENAME = "../poem/voice{0}.wav".format(self.user_num)
+                        self.user_num = (self.user_num + 1) % 3
 
-                    if event.button == 3: #right click
-                        self.mode = "busy"
-                        poem = pygame.mixer.Sound("../poem/Dimitar_Poem.wav")
-                        poem.play()
-                        self.button = 'Dimitar'
-                        print(self.button)
-                        self.mode = "play"
+                        p = pyaudio.PyAudio()
+
+                        stream = p.open(format=FORMAT,
+                                        channels=CHANNELS,
+                                        rate=RATE,
+                                        input=True,
+                                        frames_per_buffer=CHUNK) #buffer
+
+                        print("* recording")
+
+                        frames = []
+
+                        done = False
+                        while(done == False):
+                            data = stream.read(CHUNK)
+                            frames.append(data)
+                            for newevent in pygame.event.get():
+                                if newevent.type == KEYDOWN and newevent.key == pygame.K_g:
+                                    done = True
+
+                        print ("finished recording")
+                        stream.stop_stream()
+                        stream.close()
+                        p.terminate()
+
+                        wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+                        wf.setnchannels(CHANNELS)
+                        wf.setsampwidth(p.get_sample_size(FORMAT))
+                        wf.setframerate(RATE)
+                        wf.writeframes(b''.join(frames))
+                        wf.close()  
+
+                        self.mode = "play"              
                         self.start_time = time.time()
 
 
-                if event.type == KEYDOWN:
+        if self.mode == "play":
+            if  event.type == pygame.MOUSEBUTTONDOWN:
 
-                    if event.key == pygame.K_d:
-                        self.mode = "busy"
-                        self.button = 'Start'
-                        print('Intro')
-                        sound = pygame.mixer.Sound("../poem/Intro.wav")
-                        sound.play()
-                        self.mode = "record"
-                        self.start_time = time.time()
+                if event.button == 1: #left click
+                    poem = pygame.mixer.Sound("../poem/Pinar_Poem.wav")
+                    poem.play()
+                    self.button = 'Pinar'
+                    print(self.button)
+                    time.sleep(poem.get_length())
+                    self.start_time = time.time()                    
 
-                    if event.key == pygame.K_UP:
-                        self.mode = "busy"
-                        poem = pygame.mixer.Sound("../poem/voice0.wav")
-                        poem.play()
-                        self.button = 'V1'
-                        print(self.button)
-                        self.mode = "play"
-                        self.start_time = time.time()                    
+                if event.button == 3: #right click
+                    poem = pygame.mixer.Sound("../poem/Dimitar_Poem.wav")
+                    poem.play()
+                    self.button = 'Dimitar'
+                    print(self.button)
+                    time.sleep(poem.get_length())
+                    self.start_time = time.time()
 
-                    if event.key == pygame.K_LEFT:
-                        self.mode = "busy"
-                        poem = pygame.mixer.Sound("../poem/voice1.wav")
-                        poem.play()
-                        self.button = 'V2'
-                        print(self.button)
-                        self.mode = "play"
-                        self.start_time = time.time()                                    
 
-                    if event.key == pygame.K_DOWN:
-                        self.mode = "busy"
-                        poem = pygame.mixer.Sound("../poem/Bruce_Poem.wav")
-                        poem.play()
-                        self.button = 'Bruce'
-                        print(self.button)
-                        self.mode = "play"
-                        self.start_time = time.time()         
+            if event.type == KEYDOWN:
 
-                    if event.key == pygame.K_w:
-                        self.mode = "busy"
-                        poem = pygame.mixer.Sound("../poem/James_Poem.wav")
-                        poem.play()
-                        self.button = 'James'
-                        print(self.button)
-                        self.mode = "play"
-                        self.start_time = time.time()
+                if event.key == pygame.K_d:
+                    self.button = 'Start'
+                    print('Intro')
+                    sound = pygame.mixer.Sound("../poem/Intro.wav")
+                    sound.play()
+                    time.sleep(sound.get_length())              
+                    self.mode = "record"
+                    self.start_time = time.time()
 
-                    if event.key == pygame.K_s:
-                        self.mode = "busy"
-                        poem = pygame.mixer.Sound("../poem/Jee_Poem.wav")
-                        poem.play()
-                        self.button = 'Jee'
-                        print(self.button)
-                        self.mode = "play"
-                        self.start_time = time.time()                    
+                if event.key == pygame.K_UP:
+                    poem = pygame.mixer.Sound("../poem/voice0.wav")
+                    poem.play()
+                    self.button = 'V1'
+                    print(self.button)
+                    time.sleep(poem.get_length())
+                    self.start_time = time.time()                    
 
-                    if event.key == pygame.K_a:
-                        self.mode = "busy"
-                        poem = pygame.mixer.Sound("../poem/voice2.wav")
-                        poem.play()
-                        self.button = 'V3'
-                        print(self.button)
-                        self.mode = "play"
-                        self.start_time = time.time()                    
+                if event.key == pygame.K_LEFT:
+                    poem = pygame.mixer.Sound("../poem/voice1.wav")
+                    poem.play()
+                    self.button = 'V2'
+                    print(self.button)
+                    time.sleep(poem.get_length())
+                    self.start_time = time.time()                                    
+
+                if event.key == pygame.K_DOWN:
+                    poem = pygame.mixer.Sound("../poem/Bruce_Poem.wav")
+                    poem.play()
+                    self.button = 'Bruce'
+                    print(self.button)
+                    time.sleep(poem.get_length())
+                    self.start_time = time.time()         
+
+                if event.key == pygame.K_w:
+                    poem = pygame.mixer.Sound("../poem/James_Poem.wav")
+                    poem.play()
+                    self.button = 'James'
+                    print(self.button)
+                    time.sleep(poem.get_length())
+                    self.start_time = time.time()
+
+                if event.key == pygame.K_s:
+                    poem = pygame.mixer.Sound("../poem/Jee_Poem.wav")
+                    poem.play()
+                    self.button = 'Jee'
+                    print(self.button)
+                    time.sleep(poem.get_length())
+                    self.start_time = time.time()                    
+
+                if event.key == pygame.K_a:
+                    poem = pygame.mixer.Sound("../poem/voice2.wav")
+                    poem.play()
+                    self.button = 'V3'
+                    print(self.button)
+                    time.sleep(poem.get_length())
+                    self.start_time = time.time()                    
 
 
                 
@@ -220,11 +211,12 @@ if __name__ == '__main__':
         if d_time > 45:
             controller.mode = "intro"
 
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                running = False
-            controller.handle_pygame_event(event)
-            start_time = time.time()
-        time.sleep(.001)
+        if controller.mode != "busy":
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    running = False
+                controller.handle_pygame_event(event)
+                start_time = time.time()
+            time.sleep(.001)
 
     pygame.quit()
